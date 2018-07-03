955001c46fb9d7e608eec45d6a6307c9f1b1320c
yalayas <yal@yal.spb.ru>
2018-07-03 16:39:36 +0300

    Коммит для пула.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {AuthService} from './services/auth-service.service';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {AngularFontAwesomeModule} from 'angular-font-awesome';
 
 import { AppComponent } from './app.component';
 import { HeaderTopComponent } from './header-top/header-top.component';
@@ -9,15 +14,10 @@ import { AdvancedSearchComponent } from './advanced-search/advanced-search.compo
 import { PersonalInformationComponent } from './panel/personal-information/personal-information.component';
 import { ForgotPasswordComponent } from './entrance/forgot-password/forgot-password.component';
 import { LoginComponent } from './entrance/login/login.component';
-import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
-import {AuthService} from './services/auth-service.service';
-import {HttpClientModule} from '@angular/common/http';
 import { RegisterEmployerComponent } from './entrance/registration/register-employer/register-employer.component';
 import { RegisterEmployeeComponent } from './entrance/registration/register-employee/register-employee.component';
 import { RulesComponent } from './rules/rules.component';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {AngularFontAwesomeModule} from 'angular-font-awesome';
 import {Controlpanel1Component} from './panel/controlpanel1/controlpanel1.component';
 import {Controlpanel2Component} from './panel/controlpanel2/controlpanel2.component';
 
@@ -25,12 +25,14 @@ import {Controlpanel2Component} from './panel/controlpanel2/controlpanel2.compon
 const appRoutes: Routes = [
   { path: '', component: HeaderTopComponent },
   { path: 'login', component: LoginComponent },
+  {path: 'advanced-search', component: AdvancedSearchComponent},
   { path: 'register', component: RegisterComponent},
   { path: 'employee', component: RegisterEmployeeComponent},
   { path: 'employer', component: RegisterEmployerComponent},
   {path: 'forgot-password', component: ForgotPasswordComponent},
   {path: 'controlpanel1', component: Controlpanel1Component},
   {path: 'controlpanel2', component: Controlpanel2Component},
+  {path: 'rules', component: RulesComponent},
   { path: '**', component: NotFoundComponent }
 ];
 
@@ -52,6 +54,7 @@ const appRoutes: Routes = [
     RegisterEmployerComponent,
     Controlpanel1Component,
     Controlpanel2Component,
+    RulesComponent,
     NotFoundComponent
    ],
   providers: [AuthService],
