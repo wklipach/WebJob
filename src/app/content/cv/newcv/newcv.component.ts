6db9d5b9858499865ad7df82a98e1e661138414b
Klipach <klipach@mail.ru>
2018-08-27 14:17:07 +0300

    некая правка

--- src/app/content/cv/newcv/newcv.component.ts
+++ src/app/content/cv/newcv/newcv.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormGroup} from '@angular/forms';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-newcv',
@@ -10,7 +10,31 @@ export class NewcvComponent implements OnInit {
 
   newCVForm : FormGroup;
 
-  constructor() { }
+  constructor() {
+
+
+    this.newCVForm = new FormGroup({
+      'name': new FormControl('',[]),
+      'surname': new FormControl('',[]),
+      'dateBirth': new FormControl('',[]),
+      'inputSalaryFrom': new FormControl('',[]),
+      'position': new FormControl('',[]),
+      'startDate': new FormControl('',[]),
+      'completionDate': new FormControl('',[]),
+      'company': new FormControl('',[]),
+      'previousPosition': new FormControl('',[]),
+      'inputPositionDescription': new FormControl('',[]),
+      'inputPositionDescription2': new FormControl('',[]),
+      'inputSkillsAbilities': new FormControl('',[]),
+      'startDate2': new FormControl('',[]),
+      'completionDate2': new FormControl('',[]),
+      'company2': new FormControl('',[]),
+      'previousPosition2': new FormControl('',[]),
+      'inputSkillsAbilities2': new FormControl('',[])
+    });
+
+
+  }
 
   ngOnInit() {
   }
