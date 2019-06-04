370cbc28f356cf1e05789fbf3134187cc179fbd0
Klipach <klipach@mail.ru>
2019-06-04 13:29:18 +0300

    a2

--- src/app/smain/smain.component.ts
+++ src/app/smain/smain.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../services/auth-service.service';
+import { Location } from "@angular/common";
 
 @Component({
   selector: 'app-smain',
@@ -12,13 +13,17 @@ export class SmainComponent implements OnInit {
   private id_user = -1;
   protected bEmployer = false;
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService) {
+
+  }
 
   ngOnInit() {
     var Res =  this.authService.loginStorage();
     this.bConnected = Res.bConnected;
     this.id_user =  Res.id_user;
     this.bEmployer = Res.bEmployer;
+
+
   }
 
 }
