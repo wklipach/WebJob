0d7aedb8cd902db5b03fd09c382730e8ebb1268f
yalayas <yal@yal.spb.ru>
2019-07-08 17:15:46 +0300

    Координация 23

--- src/app/direct/text-to-html-directive.directive.spec.ts
+++ src/app/direct/text-to-html-directive.directive.spec.ts
@@ -3,7 +3,7 @@ import {ElementRef} from '@angular/core';
 
 describe('TextToHtmlDirectiveDirective', () => {
   it('should create an instance', () => {
-    //const directive = new TextToHtmlDirective();
-    //expect(directive).toBeTruthy();
+    // const directive = new TextToHtmlDirective();
+    // expect(directive).toBeTruthy();
   });
 });
