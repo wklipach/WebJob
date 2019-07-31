45c11077e2e931d5420c6e947e49e2ae85f21a22
Klipach <klipach@mail.ru>
2019-07-31 15:50:03 +0300

    a39

--- src/app/services/globalref.ts
+++ src/app/services/globalref.ts
@@ -1,5 +1,8 @@
 export class GlobalRef {
-     public sUrlGlobal = 'http://127.0.0.1:3001/';
+
+ // public sUrlGlobal = 'http://95.214.62.208:3000/';
+
+  public sUrlGlobal = 'http://127.0.0.1:3001/';
   // public sUrlGlobal = 'http://192.168.200.176:3001/';
 
 }
