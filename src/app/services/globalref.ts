806cd0a8a08818cb7a4547887d92de9bcb32f384
yalayas <yal@yal.spb.ru>
2019-08-28 13:54:02 +0300

    Синхронизация 25

--- src/app/services/globalref.ts
+++ src/app/services/globalref.ts
@@ -5,5 +5,11 @@ export class GlobalRef {
   //   public sUrlGlobal = 'http://127.0.0.1:3001/';
 
   public sUrlGlobal = 'http://192.168.200.176:3001/';
-   public sUrlAvatarGlobal = this.sUrlGlobal + 'images/useravatar/';
+  public sUrlAvatarGlobal = this.sUrlGlobal + 'images/useravatar/';
 }
+
+
+
+// this.sUrlGlobal="http://localhost:3000/"
+// this.sUrlGlobal="/api/"
+// public sUrlAvatarGlobal = this.sUrlGlobal + 'images/useravatar/';
