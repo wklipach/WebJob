62876f2deeedface426f2b6c430cef865a3a7aca
Yalayas <yal@yal.spb.ru>
2019-10-02 02:08:02 +0300

    Синхронизация 20

--- src/app/services/globalref.ts
+++ src/app/services/globalref.ts
@@ -2,9 +2,9 @@ export class GlobalRef {
 
   //  public sUrlGlobal = 'http://95.214.62.208:3000/';
 
-  //   public sUrlGlobal = 'http://127.0.0.1:3001/';
+     public sUrlGlobal = 'http://127.0.0.1:3001/';
 
-   public sUrlGlobal = 'http://192.168.200.176:3001/';
+  //  public sUrlGlobal = 'http://192.168.200.176:3001/';
   public sUrlAvatarGlobal = this.sUrlGlobal + 'images/useravatar/';
 }
 
