fb5e387ddf768dad72a761d422ed58c481754f7a
yalayas <yal@yal.spb.ru>
2019-10-22 17:20:39 +0300

    Done 11

--- src/app/services/globalref.ts
+++ src/app/services/globalref.ts
@@ -2,13 +2,12 @@ export class GlobalRef {
 
   //     public sUrlGlobal = 'http://95.214.62.208:3000/';
 
-      public sUrlGlobal = 'http://127.0.0.1:3001/';
+  //        public sUrlGlobal = 'http://127.0.0.1:3001/';
 
-  //       public sUrlGlobal = 'http://192.168.200.176:3001/';
+    public sUrlGlobal = 'http://192.168.200.176:3001/';
 
   public sUrlAvatarGlobal = this.sUrlGlobal + 'images/useravatar/';
 }
-
 
 
 // this.sUrlGlobal="http://localhost:3000/"
